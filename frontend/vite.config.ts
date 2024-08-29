3cdbd7b3634cbf33ade92016e101f5fb9b6df0c8
Rick <ricleyreis@gmail.com>
2024-08-29 04:51:40 -0300

    FIX: tentando fazer o Render ler a variavel de ambiente

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,6 +15,12 @@ export default defineConfig({
   },
   server:{
     host: true,
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api':{
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+      }
+    }
   }
 })
