45eaf62e78b7abe8ebc90286e8d4ad93e84351ac
Rick <ricleyreis@gmail.com>
2024-08-29 14:30:22 -0300

    ajustando vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server:{
-    host: true,
-    port: 5173,
+export default defineConfig(({ mode }) => {
+  // Carrega as variáveis de ambiente com base no modo (development, production, etc.)
+  const env = loadEnv(mode, process.cwd())
 
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: true,
+      port: 5173,
+    },
+    define: {
+      // Expõe as variáveis de ambiente para o código Vue
+      'process.env': env,
+    }
   }
 })
