478903a546417cfb846dd90c01559ad88183e1d5
Rick <ricleyreis@gmail.com>
2024-08-29 17:35:57 -0300

    ajustando bootstrap imports

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
