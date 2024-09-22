fbf90f48aaeaf822424d54a495e56b92b0a19c21
Rick <ricleyreis@gmail.com>
2024-09-22 17:14:58 -0300

    REFACTOR: ajustando script para criar bd, para funcionar no Render

--- database/create-database.sql
+++ database/create-database.sql
@@ -1,9 +1,8 @@
-DO $$
+DO $$ 
 BEGIN
     -- Verifica se o banco de dados já existe
     IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'meubanco') THEN
         -- Cria o banco de dados se não existir
-        PERFORM pg_catalog.create_database('meubanco');
+        EXECUTE 'CREATE DATABASE meubanco';
     END IF;
-END
-$$;
+END $$;
