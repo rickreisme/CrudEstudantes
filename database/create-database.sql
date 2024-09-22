7986e9a3e6a4096a2bb53fcd43c26c76f82f541a
Rick <ricleyreis@gmail.com>
2024-09-22 17:27:22 -0300

    REFACTOR: removendo pasta database

--- database/create-database.sql
+++
@@ -1,8 +0,0 @@
-DO $$ 
-BEGIN
-    -- Verifica se o banco de dados já existe
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'meubanco') THEN
-        -- Cria o banco de dados se não existir
-        EXECUTE 'CREATE DATABASE meubanco';
-    END IF;
-END $$;
